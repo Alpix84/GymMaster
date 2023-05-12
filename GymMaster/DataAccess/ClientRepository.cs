5e8541bdad7131d39522efba26a5059b16eb5476
AlpiX Gamer <alpar0184@gmail.com>
2023-05-12 15:41:56 +0300

    Clients query from database managed, MainWindow built for testing

--- GymMaster/DataAccess/ClientRepository.cs
+++ GymMaster/DataAccess/ClientRepository.cs
@@ -1,6 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using GymMaster.Models;
+
 namespace GymMaster.DataAccess;
 
 public class ClientRepository
 {
+    private readonly string _connectionString;
+
+    public ClientRepository()
+    {
+        _connectionString = Constants.ConnectionString;
+    }
+
+    public List<Client> GetAllClients()
+    {
+        var clients = new List<Client>();
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var query = "SELECT client_id, name, phoneNumber, email, isDeleted, photo, registrationDate, address, barcode, note, password FROM clients";
+
+            var command = new SqlCommand(query, connection);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var id = (int)reader["client_id"];
+                    var name = reader["name"].ToString();
+                    var phoneNumber = reader["phoneNumber"].ToString();
+                    var email = reader["email"].ToString();
+                    var isDeleted = (bool)reader["isDeleted"];
+                    var photo = reader.IsDBNull(reader.GetOrdinal("photo")) ? null : (byte[])reader["photo"];
+                    var registrationDate = reader.IsDBNull(reader.GetOrdinal("registrationDate")) ? null : (DateTime?)reader["registrationDate"];
+                    var address = reader["address"].ToString();
+                    var barcode = reader["barcode"].ToString();
+                    var note = reader["note"].ToString();
+                    var password = reader["password"].ToString();
+
+                    clients.Add(new Client(id, name, phoneNumber, email, isDeleted, photo, (DateTime)registrationDate, address, barcode, note, password));
+                }
+            }
+        }
+        return clients;
+    }
+
     
 }
--- GymMaster/Views/MainWindow.xaml.cs
+++ GymMaster/Views/MainWindow.xaml.cs
@@ -13,18 +13,22 @@ namespace GymMaster.Views
     public partial class MainWindow
     {
         private AdminRepository _adminRepository = new AdminRepository();
+        private ClientRepository _clientRepository = new ClientRepository();
         public MainWindow()
         {
             InitializeComponent();
-            List<Admin> admins = _adminRepository.GetAllAdmins();
-            AdminListTextBlock.Text = string.Join(Environment.NewLine, admins.Select(admin => $"ID: {admin.Id}, Name: {admin.Name}, Email: {admin.Email}, Phone Number: {admin.PhoneNumber}"));
-
         }
         
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             List<Admin> admins = _adminRepository.GetAllAdmins();
             AdminListTextBlock.Text = string.Join(Environment.NewLine, admins.Select(admin => $"ID: {admin.Id}, Name: {admin.Name}, Email: {admin.Email}, Phone Number: {admin.PhoneNumber}"));
+        }
+        
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            List<Client> clients = _clientRepository.GetAllClients();
+            AdminListTextBlock.Text = string.Join(Environment.NewLine, clients.Select(client => $"ID: {client.Id}, Name: {client.Name}, Email: {client.Email}, Phone Number: {client.PhoneNumber}"));
         }
 
         
