dac05d28cff5e181fd7d6c351ee1168b92c86bb7
Endre137 <balazsendre11@gmail.com>
2023-05-23 23:43:36 +0300

    UI Design

---
+++ GymMaster/Views/LoggedInAdmin/NewClientView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GymMaster.Views.LoggedInAdmin
+{
+    /// <summary>
+    /// Interaction logic for NewClientView.xaml
+    /// </summary>
+    public partial class NewClientView : UserControl
+    {
+        public NewClientView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
